3339c877ad7909921715f4bd08a4984afb3e1859
nahidul77 <nahidul.mee@gmail.com>
2022-05-01 01:26:54 +0600

    import export not copies rather live connection

--- 17 - Modern JavaScript Development Modules Tooling and Functional/05 Export & import es6 module/script.js
+++ 17 - Modern JavaScript Development Modules Tooling and Functional/05 Export & import es6 module/script.js
@@ -17,8 +17,20 @@ console.log('Importing Modules');
 // console.log(ShoppingCart.totalPrice);
 // console.log(ShoppingCart.qty);
 
-import add, { addToCart, totalPrice as price, qty } from './shoppingCart.js';
+// import add, { addToCart, totalPrice as price, qty } from './shoppingCart.js'; // Default and named export together, not recommended
+
+// add('pizza', 2);
+// console.log(price);
+// console.log(qty);
+
+// import add from './shoppingCart.js'; // Dont need to use curly braces
+
+// add('pizza', 2);
+
+import add, { cart } from './shoppingCart.js'; // live connection 
 
 add('pizza', 2);
-console.log(price);
-console.log(qty);
+add('burger', 4);
+add('Pastry', 4);
+
+console.log(cart);
--- 17 - Modern JavaScript Development Modules Tooling and Functional/05 Export & import es6 module/shoppingCart.js
+++ 17 - Modern JavaScript Development Modules Tooling and Functional/05 Export & import es6 module/shoppingCart.js
@@ -3,12 +3,13 @@
 /**
  * All the top level variable scope only respective modules.
  * In ES6 Module , there is two types of exports, named Exports and Default Exports.
+ * Import are not copies of export, they are instead like a live connection
  */
 
 console.log('Exporting Modules');
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity) {
     cart.push({ product, quantity });
