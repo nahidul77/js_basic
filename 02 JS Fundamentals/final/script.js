bf944c836ab63afd35824b42e632ac8f33a45f13
nahidul77 <nahidul.mee@gmail.com>
2022-05-05 22:35:29 +0600

    datatypes

--- 02 JS Fundamentals/final/script.js
+++ 02 JS Fundamentals/final/script.js
@@ -7,17 +7,37 @@ console.log(40 + 8 + 23 - 10);
 // Values and Variables
 console.log("Jonas");
 console.log(23);
-let firstName = "Matilda";
+let firstName = "Matilda"; // camelcase would be naming convention
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
 // Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 let person = "jonas";
-let PI = 3.1415;
+let PI = 3.1415; //constant name will uppercase
+
+// Descriptive name
 let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 let job1 = "programmer";
 let job2 = "teacher";
 console.log(myFirstJob);
+
+////////////////////////////////////
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun); // string
+let year;
+console.log(year);
+console.log(typeof year); // undefined
+year = 1991;
+console.log(typeof year); // number
+console.log(typeof null); // object , which is js bug
