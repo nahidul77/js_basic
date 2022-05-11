c6bdb7ce690a2e0adc107df32a042ab7f5619898
nahidul77 <nahidul.mee@gmail.com>
2022-05-11 15:45:57 +0600

    The ternary operator

--- 02 JS Fundamentals/final/script.js
+++ 02 JS Fundamentals/final/script.js
@@ -377,4 +377,25 @@ if (23 > 10) {
 const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}`);
 
+
+////////////////////////////////////
+// The Conditional (Ternary) Operator
+// Operator alwasys produce a value
+// its not replacement of if else rather quick decision making.
+
+const age = 23;
+// age >= 18 ? console.log('I like to drink tea ') : console.log('I like to drink water');
+const drink = age >= 18 ? 'tea' : 'water';
+console.log(drink);
+let drink2;
+if (age >= 18) {
+  drink2 = 'tea';
+} else {
+  drink2 = 'water';
+}
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? 'tea' : 'water'}`);
+*/
+
+
 */
